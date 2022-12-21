24ed5f47d64c6fb9e7c2850c35883beb444d8339
S.R. Shuvo <shuvo22890@gmail.com>
2022-12-21 21:26:01 +0600

    Lesson 9 contents

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp({
+  apiKey: "AIzaSyBD6OrpZfa66rvv_ur2EzoreHaVU4OqSrA",
+  authDomain: "react-quiz-dev-f7ff1.firebaseapp.com",
+  projectId: "react-quiz-dev-f7ff1",
+  storageBucket: "react-quiz-dev-f7ff1.appspot.com",
+  messagingSenderId: "140633202891",
+  appId: "1:140633202891:web:605875388476c70551fb4e",
+});
+
+export default app;
